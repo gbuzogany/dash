465f85a8dd89eb9ed1b5cf294e1b27c9ac448866
Gustavo <gbuzogany@gmail.com>
2019-08-13 14:42:13 +0200

    Initial Commit

---
+++ dash/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dash
+//
+//  Created by Gustavo Buzogany Eboli on 13/08/2019.
+//  Copyright © 2019 gbuzogany.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
