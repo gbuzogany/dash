e03e9295f517820040bf6ea2a49a56335a7ca656
Gustavo Buzogany Eboli <gbuzogany@gmail.com>
2020-05-18 18:11:20 +0200

    Added integration example Updated integration readme

--- INTEGRATION.md
+++ INTEGRATION.md
@@ -2,4 +2,12 @@
 
 ## Integration
 
-WIP
+If you are not familiar with gRPC, it's a great idea to go through [their documentation](https://grpc.io/docs/quickstart/).
+
+The `proto` file that describes the gRPC interfaces available on Rockette is `src/proto/dash.proto`.
+
+By default, the `MediaPlayer` service binds to all interfaces on port `50051`, and the `DashControl` service on port `50052`, but this can be changed in the `config.json` file.
+
+## Examples
+
+There are basic integration examples in the `integration/examples` directory.
---
+++ integration/examples/python/dash-control/DashClient.py
@@ -0,0 +1,18 @@
+import grpc
+import dash_pb2
+import dash_pb2_grpc
+
+class DashClient:
+	def __init__(self, config):
+		self.config = config
+		self.host_string = '{0}:{1}'.format(self.config['host'], self.config['port'])
+
+	def UpdateDashData(self, dashData):
+		with grpc.insecure_channel(self.host_string) as channel:
+			stub = dash_pb2_grpc.DashControlStub(channel)
+			response = stub.UpdateDashData(dashData)
+
+	def UpdateDashExtendedData(self, dashExtendedData):
+		with grpc.insecure_channel(self.host_string) as channel:
+			stub = dash_pb2_grpc.DashControlStub(channel)
+			response = stub.UpdateDashExtendedData(dashExtendedData)
